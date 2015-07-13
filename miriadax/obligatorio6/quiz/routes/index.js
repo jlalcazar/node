f197886ed7c861d33a981cbb224ac5567b7752ff
jlalcazar <joseluisalcazar@gmail.com>
2015-07-13 23:56:00 +0200

    Last momment push

--- miriadax/obligatorio6/quiz/routes/index.js
+++ miriadax/obligatorio6/quiz/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+
+var quizController = require('../controllers/quiz_controller');
+
+
+router.get('/author', function (req, res) {
+	res.render('author', {
+		title: 'Author'
+	})
 });
+
+/* GET home page. */
+router.get('/', function (req, res) {
+	res.render('index', {
+		title: 'Quiz'
+	});
+});
+
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
